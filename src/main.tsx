46ccff6c9c6a2f8e02f5a8185e3e49e9584c263d
dpserretti <danielserretti@gmail.com>
2026-01-24 17:30:33 -0300

    chore: add tailwindcss and configure base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import { Providers } from '@/app/providers'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/app/router'
